a1eeb5f42fd47a9812b5471918c9ac14ef23e346
Ashok Thakur <iashok180@gmail.com>
2019-07-30 23:22:28 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+from tornado.web import RequestHandler, Application
+import tornado.ioloop
+import os.path
+import data
+import json
+
+
+class MainHandler(RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+class DataHandler(RequestHandler):
+    def get(self):
+        data = data.read_data()
+        self.write({'response': json.loads(data)})
+
+settings = dict(
+    template_path = os.path.join(os.path.dirname(__file__),'templates'),
+    # static_path = os.path.join(os.path.dirname(__file__),'static'),
+    debug=True
+)
+
+
+def make_app():
+    return Application(
+    [
+        (r'/', MainHandler),
+        (r'/data', DataHandler),
+        (r'/(.*)', tornado.web.StaticFileHandler, {"path": ""}),
+
+    ],**settings)
+
+
+if __name__ == '__main__':
+    print("Server is running at 9000")
+    app = make_app()
+    app.listen(9000)
+    tornado.ioloop.IOLoop.current().start()
