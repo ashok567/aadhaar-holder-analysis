6b41464e366817dee0c6a15a054c4ccfbcf710b1
Ashok Thakur <iashok180@gmail.com>
2019-08-25 23:53:28 +0530

    Map and navbar style changed

--- data.py
+++ data.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import json
+import sqlite3
 
 def read_data():
-    df = pd.read_csv('data/aadhaar_data.csv')
+    conn = sqlite3.connect('data/aadhaar_stats.db')
+    df = pd.read_sql('select * from state_counts', conn)
     df = df.to_json(orient='records')
+    conn.close()
     return df
+
+
